196c24b270629aaf3b6282dee68eae434cff20a3
ogibacw <bachwilliam17@gmail.com>
2025-07-13 21:15:11 -0400

    New test

--- quiz_db.py
+++ quiz_db.py
@@ -4,7 +4,7 @@ def generate_quiz(database) :
     conn = sqlite3.connect(database)
     table_name = database.replace(".db", "")
     
-    SQL_request = f"SELECT * from {table_name} LIMIT 5"
+    SQL_request = f"SELECT * from {table_name} LIMIT 6"
     current_line = conn.execute(SQL_request).fetchall()
     print(current_line)
 
